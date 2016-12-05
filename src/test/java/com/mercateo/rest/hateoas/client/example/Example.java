21e616020a6402b4bf03ae3c308769755285bff6
joerg.adler <joerg.adler@mercateo.com>
2016-12-05 11:00:44 +0100

    Added example for link templates

--- src/test/java/com/mercateo/rest/hateoas/client/example/Example.java
+++ src/test/java/com/mercateo/rest/hateoas/client/example/Example.java
@@ -6,22 +6,48 @@ import com.mercateo.rest.hateoas.client.ClientStarter;
 import com.mercateo.rest.hateoas.client.ListResponse;
 import com.mercateo.rest.hateoas.client.Response;
 
-public class Example {
-	public static void main(String[] args) {
-		Response<Object> one = new ClientStarter().create("http://localhost:9090", Object.class);
-		Optional<ListResponse<OrderProjectionJson>> two = one.prepareNextWithResponse(OrderProjectionJson.class)
-				.callListWithRel("orders");
+import lombok.Value;
 
-		System.out.println(two.get().get(0).get().getResponseObject());
+public class Example {
+
+	@Value
+	public static class IdBean {
+		String id;
+	}
+
+	public static void main(String[] args) {
+		Response<Object> rootResource = new ClientStarter().create("http://localhost:9090", Object.class);
+		Optional<ListResponse<OrderProjectionJson>> collectionResource = rootResource
+				.prepareNextWithResponse(OrderProjectionJson.class).callListWithRel("orders");
+
 		SendBackJson sendBackJson = new SendBackJson("test");
 
-		Optional<Response<Void>> three = two.get().get(1).get().prepareNextWithResponse(Void.class)
+		Optional<Response<OrderProjectionJson>> secondListElementResource = collectionResource.get().get(1);
+		System.out.println(secondListElementResource.get().getResponseObject());
+		Optional<Response<Void>> sentBack1 = secondListElementResource.get().prepareNextWithResponse(Void.class)
 				.withRequestObject(sendBackJson).callWithRel("send-back");
 
-		if (three.isPresent()) {
-			System.out.println("sent back");
+		if (sentBack1.isPresent()) {
+			System.out.println("sent back in list");
 		} else {
-			System.out.println("no send-back available");
+			System.out.println("no send-back available in list available");
 		}
+
+		// demonstrating templated links
+		Optional<Response<OrderProjectionJson>> secondElementDirectCall = collectionResource.get()
+				.prepareNextWithResponse(OrderProjectionJson.class).withRequestObject(new IdBean("2"))
+				.callWithRel("instance");
+
+		System.out.println(secondElementDirectCall.get().getResponseObject());
+
+		Optional<Response<Void>> sentBack2 = secondElementDirectCall.get().prepareNextWithResponse(Void.class)
+				.withRequestObject(sendBackJson).callWithRel("send-back");
+
+		if (sentBack2.isPresent()) {
+			System.out.println("sent back in template");
+		} else {
+			System.out.println("no send-back available in template");
+		}
+
 	}
 }
